52c02bbca509d0223faeb1c22619056abe5e08c4
Danilo Silva <danilo.silva@msn.com>
2024-11-24 22:01:31 -0300

    adiciona componente de cabeçalho e implementa formulário de login com validação

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  senhaEscondida = true;
 
+  formularioLogin = this.formBuilder.group({
+    chave: ['', Validators.required],
+    senha: ['', Validators.required],
+  });
+
+  constructor(
+    private autenticacaoService: AutenticacaoService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if (
+      this.autenticacaoService.possuiToken() &&
+      this.autenticacaoService.estaAutenticado()
+    ) {
+      this.router.navigate(['/home']);
+    }
+  }
+
+  aoLogar(): void {
+    if (this.formularioLogin.valid) {
+      this.autenticacaoService
+        .autenticar(this.formularioLogin.value)
+        .subscribe(() => {
+          this.router.navigate(['/home']);
+        });
+    }
+  }
 }
